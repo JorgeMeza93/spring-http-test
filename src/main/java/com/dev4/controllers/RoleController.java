83a13c30826843bc7bc67dc8883417d5ff31afa3
Jorge Meza Bello <jorberiver@gmail.com>
2023-04-05 23:59:42 -0600

    Crreado put role

--- src/main/java/com/dev4/controllers/RoleController.java
+++ src/main/java/com/dev4/controllers/RoleController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +30,9 @@ public class RoleController {
 	@PostMapping
 	public ResponseEntity<Role> createRole(@RequestBody Role role){
 		return new ResponseEntity<Role>(service.createRole(role), HttpStatus.CREATED);
+	}
+	@PutMapping("/{roleId}")
+	public ResponseEntity<Role> updateRole(@PathVariable("roleId") Integer id, @RequestBody Role role){
+		return null;	
 	}
 }
