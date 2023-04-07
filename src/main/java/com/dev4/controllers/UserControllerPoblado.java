c1f5d59eaf710e5f117a0a686e496310de6c69d9
Jorge Meza Bello <jorberiver@gmail.com>
2023-04-06 23:56:18 -0600

    Find by Id listo

--- src/main/java/com/dev4/controllers/UserControllerPoblado.java
+++ src/main/java/com/dev4/controllers/UserControllerPoblado.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +22,9 @@ public class UserControllerPoblado {
 	public ResponseEntity<List<User>> getUsers(){
 		return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
 	}
+	@GetMapping("/{userId}")
+	public ResponseEntity<User> getUserById(@PathVariable("userId") Integer userId){
+		return new ResponseEntity<User>(userService.getUserById(userId), HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/dev4/controllers/UserServicePoblado.java
+++ src/main/java/com/dev4/controllers/UserServicePoblado.java
@@ -3,7 +3,9 @@ package com.dev4.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.dev4.entities.User;
 import com.dev4.repositories.UserRepository;
@@ -16,5 +18,11 @@ public class UserServicePoblado {
 	
 	public List<User> getUsers(){
 		return userRepository.findAll();
+	}
+	
+	public User getUserById(Integer id) {
+		return userRepository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, 
+				String.format("Usuario con id %d no encontrado", id)) );
+		
 	}
 }
