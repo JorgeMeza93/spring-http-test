2dc6413b2e7967defe514bbfc07d723e4b18a3c0
Jorge Meza Bello <jorberiver@gmail.com>
2023-04-10 22:35:20 -0600

    Añadidas metricas y actuator

--- src/main/java/com/dev4/controllers/UserControllerPoblado.java
+++ src/main/java/com/dev4/controllers/UserControllerPoblado.java
@@ -13,16 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dev4.entities.User;
 import com.dev4.services.UserServicePoblado;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+
 @RestController
 @RequestMapping("/poblados/usuarios")
 public class UserControllerPoblado {
 	@Autowired
 	private UserServicePoblado userService;
 	
+	private final ObservationRegistry observationRegistry;
+	 
+	public UserControllerPoblado(ObservationRegistry observationRegistry) {
+	    this.observationRegistry = observationRegistry;
+	}
+	
 	@GetMapping
+	@Timed("getusers")
 	public ResponseEntity<Page<User>> getUsers(@RequestParam(required = false, value="page", defaultValue = "0") 
 		int page, @RequestParam(required = false, value  = "size", defaultValue = "5") int size){
-		return new ResponseEntity<Page<User>>(userService.getUsers(page, size), HttpStatus.OK);
+		//return new ResponseEntity<Page<User>>(userService.getUsers(page, size), HttpStatus.OK);
+		
+		return Observation.createNotStarted("get-users", observationRegistry).observe(() -> {
+	        return new ResponseEntity<>(userService.getUsers(page, size), HttpStatus.OK);
+	    });
 	}
 	/* Query Personalizado" */
 	@GetMapping("/usernames")
